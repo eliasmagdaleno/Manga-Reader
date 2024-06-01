5efbf1dc8e0114bdf93f62ce248d2efc85418e19
Elias Magdaleno <eliasrmagdaleno@gmail.com>
2024-05-31 17:03:35 -0700

    Initial Commit

---
+++ Manga-Reader/Manga_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Manga_ReaderApp.swift
+//  Manga-Reader
+//
+//  Created by Elias Magdaleno on 5/31/24.
+//
+
+import SwiftUI
+
+@main
+struct Manga_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
