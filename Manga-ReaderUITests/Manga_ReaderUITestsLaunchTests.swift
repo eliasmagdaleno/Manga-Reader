057cb9ebe7c14f8a226c1ab58dd17a6bb8e23ae1
eliasmagdaleno <eliasrmagdaleno@gmail.com>
2026-08-24 23:09:08 -0700

    Fix strict lint in UI tests

--- Manga-ReaderUITests/Manga_ReaderUITestsLaunchTests.swift
+++ Manga-ReaderUITests/Manga_ReaderUITestsLaunchTests.swift
@@ -9,7 +9,7 @@ import XCTest
 
 final class Manga_ReaderUITestsLaunchTests: XCTestCase {
 
-    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+    override static var runsForEachTargetApplicationUIConfiguration: Bool {
         true
     }
 
